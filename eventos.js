0cf418b636c74e02d83a4841a6c60669be1a0461
Renato Carabelli <renatocarabellinavarro@gmail.com>
2023-09-03 19:26:50 -0300

    first commit

---
+++ eventos.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+    const dContenedor = document.getElementById("contenedor");
+    const boton = document.getElementById("saludarBoton");
+    
+    
+    dContenedor.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+    
+    
+    
+    
+    boton.addEventListener("click", function() {
+        alert("Hola! ");
+    });
+    
+    
+    
+    
+       
+    });
+    
